45031d8609c446bb53b1223ddfbb56016d18121f
IvanCuradaFrustratedCoder <val.curada@globe.com.ph>
2021-10-20 20:58:14 +0800

    Initialized Firebase App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { getFunctions, httpsCallable } from 'firebase/functions';
+import { getApp } from 'firebase/app';
+
 function App() {
+
+  const getMessage = async () => {
+    try {
+      const functions = getFunctions(getApp(), 'asia-east2');
+      const helloWorld = httpsCallable(functions, 'helloWorld');
+      const res = await helloWorld({ data: 'asd' });
+      const data = res.data;
+
+      console.log(data);
+
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  React.useEffect(() => {
+    getMessage();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from 'firebase/app';
+import { getFunctions } from 'firebase/functions';
+
+export const app = initializeApp({
+  apiKey: "AIzaSyDjmhsg2w2GzRXHuj9FVnuzQovVwKGY5aw",
+  authDomain: "ivan-labs.firebaseapp.com",
+  databaseURL: "https://ivan-labs.firebaseio.com",
+  projectId: "ivan-labs",
+  storageBucket: "ivan-labs.appspot.com",
+  messagingSenderId: "443977517502",
+  appId: "1:443977517502:web:b79f39e2d5b058b55311ef"
+});
+
+export const functions = getFunctions(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
